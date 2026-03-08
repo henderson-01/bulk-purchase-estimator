0fbd8c6b40422a759cd3a36e0f806b91626e4bcb
Henderson-01 <261472068+henderson-01@users.noreply.github.com>
2026-03-08 19:26:54 +0000

    Added .strip & .lower to line 1

--- estimator.py
+++ estimator.py
@@ -1,9 +1,8 @@
-item = input("\nWhat would you like to buy?: ")
+item = input("\nWhat would you like to buy?: ").strip().lower()
 
 while True:
     try:
-        price = float(
-            input(f"How much is one single {item} going to cost £: "))
+        price = float(input(f"How much is one single {item} going to cost £: "))
         break
     except ValueError:
         print("Error: Please enter the price using numbers (e.g., 3.75)")
